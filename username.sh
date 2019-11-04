b040d0abe85210436b33fb750c57214d2224221f
jhburns <jburns@chapman.edu>
2019-11-04 10:25:20 -0800

    Testing

---
+++ username.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# zip.sh
+# author: Jonathan Burns
+
+echo "Requirements for a username: "
+echo "- only lower case letters,"
+echo "- digits,"
+echo "- underscore,"
+echo "- must start with a lower case letter,"
+echo "- contain 3 to 12 characters"
+
+echo "Enter a valid username:"
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z]{3,12}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username."
+	echo "Enter a valid username: "
+	read USERNAME
+done
+echo "Thank you"
